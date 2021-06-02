ae4425f5f1e8e81c768c9361f453f3a50669bb6d
Giedrius <paurys79@gmail.com>
2021-06-02 19:11:59 +0300

    Įkėliau pirmą ir paskutinį elementus

---
+++ script.js
@@ -0,0 +1,5 @@
+alert("Naujas dalyvis: " + name);
+
+var names = ["JonSnow", "Cersei Lannister", "Daenerys Targaryen",
+"Theon Greyjoy", "Tyrion Lannister", "Arya Stark"];
+console.log(names[0] + ", " + names[5] + ". ");
